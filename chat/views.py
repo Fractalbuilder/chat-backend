1f3df2064e4fd48c3fe22bdc7221a21a765cf438
fractalbuilder <fractalbuilder@gmail.com>
2022-01-16 12:44:27 -0500

    Commit 01-16-03

--- chat/views.py
+++ chat/views.py
@@ -9,9 +9,6 @@ from django.http import StreamingHttpResponse
 from .models import User, Message
 import datetime
 
-#originURL = 'http://127.0.0.1:3000'
-originURL = 'https://fb-chat00.herokuapp.com'
-
 def index(request):
     return render(request, "general/index.html")
 
@@ -24,9 +21,12 @@ def chat_messages(request):
             print("Info sent")
             time.sleep(1)
     
+    """
     response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
     response['Access-Control-Allow-Origin'] = originURL
     return response
+    """
+    return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
 
 @csrf_exempt
 def login(request):
@@ -39,24 +39,29 @@ def login(request):
 
         # Check if authentication successful
         if user is not None:
+            """
             response = JsonResponse({"message": "User logged in", "userId": user.id, "username": username}, status=201)
             response['Access-Control-Allow-Origin'] = 'https://fb-chat00.herokuapp.com'
             response['Access-Control-Allow-Credentials'] = 'true'
-            #response.set_cookie('userId', user.id, { sameSite: 'none', secure: true })
-            #response.set_cookie('username', username)
-            #response.set_cookie(key='userId', value=user.id, samesite='None', secure=True, httponly=False)
-            #response.set_cookie(key='username', value=username, samesite='None', secure=True)
             return response
+            """
+            return JsonResponse({"message": "User logged in", "userId": user.id, "username": username}, status=201)
         else:
+            """
             response = JsonResponse({"message": "User not logged in"}, status=201)
             response['Access-Control-Allow-Origin'] = originURL
             response['Access-Control-Allow-Credentials'] = 'true'
             return response
+            """
+            return JsonResponse({"message": "User not logged in"}, status=201)
     else:
+        """
         response = JsonResponse({"message": "Invalid opperation"}, status=201)
         response['Access-Control-Allow-Origin'] = originURL
         response['Access-Control-Allow-Credentials'] = 'true'
         return response
+        """
+        return JsonResponse({"message": "Invalid opperation"}, status=201)
 
 @csrf_exempt
 def messages_api(request):
@@ -67,10 +72,12 @@ def messages_api(request):
         text = data['text']
         date = datetime.datetime.now()        
         Message(userId=userId, username=username, text=text, datetime=str(date)).save()
+        """
         response = JsonResponse({"message": "Message added."}, status=201)
         response['Access-Control-Allow-Origin'] = 'https://fb-chat00.herokuapp.com'
         return response
-
+        """
+        return JsonResponse({"message": "Message added."}, status=201)
 
 @csrf_exempt
 def register_user(request):
@@ -82,28 +89,40 @@ def register_user(request):
         confirmation = password
 
         if password != confirmation:
+            """
             response = JsonResponse({"message": "Password doesn't match"}, status=201)
             response['Access-Control-Allow-Origin'] = originURL
             return response
+            """
+            return JsonResponse({"message": "Password doesn't match"}, status=201)
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
+            """
             response = JsonResponse({"message": "User already taken"}, status=201)
             response['Access-Control-Allow-Origin'] = originURL
             response['Access-Control-Allow-Credentials'] = 'true'
             return response
+            """
+            return JsonResponse({"message": "User already taken"}, status=201)
 
+        """
         response = JsonResponse({"message": "User added"}, status=201)
         response['Access-Control-Allow-Origin'] = originURL
         response['Access-Control-Allow-Credentials'] = 'true'
         response.set_cookie('userId', user.id)
         response.set_cookie('username', username)
         return response
+        """
+        return JsonResponse({"message": "User added"}, status=201)
     else:
+        """
         response = JsonResponse({"message": "Invalid opperation"}, status=201)
         response['Access-Control-Allow-Origin'] = originURL
         response['Access-Control-Allow-Credentials'] = 'true'
         return response
+        """
+        return JsonResponse({"message": "Invalid opperation"}, status=201)
